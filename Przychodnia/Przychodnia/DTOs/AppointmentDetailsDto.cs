8b78b506d18a48a9ed42f3b83fa4a0ba00fd7b30
Michal Fukowski <michal01f@gmail.com>
2026-04-25 18:45:01 +0200

    add DTOs

--- Przychodnia/Przychodnia/DTOs/AppointmentDetailsDto.cs
+++ Przychodnia/Przychodnia/DTOs/AppointmentDetailsDto.cs
@@ -2,7 +2,6 @@
 
 public class AppointmentDetailsDto
 {
-    // DoctorsDetails
     public string PatientEmail { get; set; } = string.Empty;
     public string PatientPhoneNumber { get; set; } = string.Empty;
     public string DoctorLicenseNumber { get; set; } = string.Empty;

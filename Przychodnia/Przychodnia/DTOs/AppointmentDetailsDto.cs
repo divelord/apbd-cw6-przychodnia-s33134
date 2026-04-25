28e86ab022e8d508760659bc3f6cebac2d143c94
Michal Fukowski <michal01f@gmail.com>
2026-04-25 18:43:27 +0200

    add DTOs

--- Przychodnia/Przychodnia/DTOs/AppointmentDetailsDto.cs
+++ Przychodnia/Przychodnia/DTOs/AppointmentDetailsDto.cs
@@ -3,20 +3,9 @@
 public class AppointmentDetailsDto
 {
     // DoctorsDetails
-    public int IdDoctor { get; set; }
-    public string DoctorFullName { get; set; } = string.Empty;
-    public string LicenseNumber { get; set; } = string.Empty;
-    public string Specialization { get; set; } = string.Empty;
-    // PatientsDetails
-    public int IdPatient { get; set; }
-    public string PatientFullName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string PhoneNumber { get; set; } = string.Empty;
-    // AppointmentsDetails
-    public int IdAppointment { get; set; }
-    public DateTime AppointmentDate { get; set; }
-    public string Status { get; set; } = string.Empty;
-    public string Reason { get; set; } = string.Empty;
+    public string PatientEmail { get; set; } = string.Empty;
+    public string PatientPhoneNumber { get; set; } = string.Empty;
+    public string DoctorLicenseNumber { get; set; } = string.Empty;
     public string? InternalNotes { get; set; }
     public DateTime CreatedAt { get; set; }
 }
